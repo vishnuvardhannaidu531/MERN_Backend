cdaa45680fb641b5a474bc4f2f697a1e623e8864
Vishnu Vardhan Naidu <vishnuvardhannaidu09@gmail.com>
2026-07-19 05:41:27 +0530

    Update backend project

--- Jwt_Tokens/Api_Level_validation/Data_santisation_and_Schema_Validation/routes/auth.js
+++ Jwt_Tokens/Api_Level_validation/Data_santisation_and_Schema_Validation/routes/auth.js
@@ -37,4 +37,17 @@ authrouter.post("/login" ,async (req ,res) =>{
     }
 })
 
+authrouter.post("/logout" ,async (req, res) => {
+    try{
+        //See to meke logout simply means  that user needs to login again or we can say that we need to make user ion such a way that use can't able to ge access 
+        //And one more thing is that as ever user request to server then use along with the request user will send the cookies and as ever cookies reaches to this server now this server will do one thing that as ever if server send any cookies then the browser o postman automatically remove the old cookie because browser will thought that as server sended new token it means that as if token name and path all these are same then  browser as all are same so browser will update the token as because toekns needs to be identify unique name so that browser an get to know to which server this browser needs to send the token to which eserver  so here server will take this as an adavantage for the logout purpose  as i will generate the randome dummy token which will be  invalid from as if user hit request with that token definitely  token will going to rejected so we user can't able to use the serveices
+        // res.cookie("token" , "afbfggfg gy egfh ") ;
+        // res.cookie("tokens" , "abcdefghijk") ;
+        res.cookie("token" , null ,{expires : new Date(Date.now())})
+        res.send("Logout Successfully")
+    }catch(err){
+        res.status(400).send("Error message :" +err.message)
+    }
+})
+
 module.exports = authrouter ; 
--- Jwt_Tokens/Api_Level_validation/Utils/Validation.js
+++ Jwt_Tokens/Api_Level_validation/Utils/Validation.js
@@ -1,3 +1,5 @@
+const validator = require("validator");
+
 function validUser(req) {
     const { fname, emailID, password } = req.body;
         
